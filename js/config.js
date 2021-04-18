8de4cdf98b60860846911a71f38479dde10e4fc4
dashton <david.marc.ashton@gmail.com>
2021-04-18 10:36:00 +0100

    update aws user pool config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-2_6BwMu6tbo', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2vo7odm09p5rsiobbi2mpfllkk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
